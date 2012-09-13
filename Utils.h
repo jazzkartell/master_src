37c7446445ac39022ae501d8372eb0734a8320ae
Benjamin Kirchholtes <jazzkartell@gmx.de>
2012-09-13 19:22:52 +0200

    -

--- Utils.h
+++ Utils.h
@@ -3,7 +3,6 @@
 #include <vector>
 #include <pcl/point_types.h>
 #include <fstream>
-#include "Point2D.h"
 #include <stdlib.h>
 
 using namespace std;
@@ -19,7 +18,7 @@ void tokenize(const string& str, vector<string>& tokens, const string& delimiter
     }
 }
 
-void readFile(const char* file, pcl::PointCloud<Point2D>* cloud){
+void readFile(const char* file, pcl::PointCloud<pcl::PointXYZL>* cloud){
 	string line;
 	ifstream in (file);
 	vector<string> tokens;
@@ -29,11 +28,11 @@ void readFile(const char* file, pcl::PointCloud<Point2D>* cloud){
 	    	if (line.empty()){continue;}
 	    	tokens.clear();
             tokenize(line, tokens, "\t \n ");
-            Point2D p;
+            pcl::PointXYZL p;
             for(int i=0; i<tokens.size();i++){
-                p.set_x(atof(tokens.at(0).c_str()));
-                p.set_y(atof(tokens.at(1).c_str()));
-                p.set_label(tokens.at(2).c_str());
+                p.x = (atof(tokens.at(0).c_str()));
+                p.y = (atof(tokens.at(1).c_str()));
+                p.z = (atof(tokens.at(2).c_str()));
             }
             cloud->push_back(p);
 		}
--- makeHash2D.cpp
+++ makeHash2D.cpp
@@ -1,15 +1,20 @@
 //#include "Utils.h"
-#include "Point2D.h"
 #include <string>
 #include <iostream>
 #include <pcl/point_types.h>
 #include "Utils.h"
+#include "pcl/common/geometry.h"
+#include "pcl/common/transformation_from_correspondences.h"
+#include "pcl/common/transforms.h"
+
 
 using namespace std;
 
+
+
 int main(int argc, char** argv) {
     
-    pcl::PointCloud<Point2D>* cloud = new pcl::PointCloud<Point2D>();
+    pcl::PointCloud<pcl::PointXYZL>* cloud = new pcl::PointCloud<pcl::PointXYZL>();
     
     // we require one file containing the coordinate file as input
     if(argc != 2){
@@ -18,9 +23,44 @@ int main(int argc, char** argv) {
         readFile(argv[1], cloud);
     }
     
-    cout << cloud->size() << endl;
+    // get all possible basis pairs
+    int basis_count = 0;
+    for(int i=0; i<cloud->size(); i++){
+        for (int j=0; j<cloud->size(); j++){
+            if(i != j){
+                basis_count++;
+                
+            }
+        }
+    }
+    cout << basis_count << " basis were produced..." << endl;
+
+    
+    const Eigen::Vector3f y_dir = Eigen::Vector3f(0.0,1.0,0.0);
+    const Eigen::Vector3f z_axis = Eigen::Vector3f(0.0,0.0,1.0);
+    const Eigen::Vector3f origin = Eigen::Vector3f(2.0,3.0,0.0);
+    
+    pcl::PointCloud<pcl::PointXYZL>* cloud2 = new pcl::PointCloud<pcl::PointXYZL>();
+    
+    Eigen::Affine3f trans;
+    pcl::getTransformationFromTwoUnitVectorsAndOrigin(y_dir, z_axis, origin, trans);
+    
+    vector<int> indices;
+    indices.push_back(0);
+    pcl::transformPointCloud(*cloud, *cloud2, trans);
+    
+    cout << cloud2->size() << endl;
+    for(int i=0; i<cloud2->size(); i++){
+        cout << cloud->at(i).getArray3fMap()[0] << " ";
+        cout << cloud->at(i).getArray3fMap()[1] << "  -->  ";
+        
+        cout << cloud2->at(i).getArray3fMap()[0] << " ";
+        cout << cloud2->at(i).getArray3fMap()[1] << endl;
+    }
+    
     
     delete cloud;
+    delete cloud2;
     return 0;
 }
 
